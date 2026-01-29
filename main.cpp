2c4b41918ce7db32e8d4c3b5e32fb185eb57671c
PhoB <hsp003636@gmail.com>
2026-01-29 18:17:40 +0900

    feat: main의 역할 최소화
    - 기존: pcap핸들 및 패킷의 전송을 모두 main에서 진행
    - 변경: engine을 실행만하고 나머지는 다른 클래스에서 진행

---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include "core/Engine.h"
+#include <iostream>
+#include <csignal>
+#include <memory>
+
+std::unique_ptr<Engine> engine = nullptr;
+void signalHandler(int signum) {
+        (void)signum; // 경고 방지
+        if (engine) {
+                std::cout << "\n[Main] Interrupt signal received. Stopping engine..." << std::endl;
+                engine->stop();
+        }
+}
+
+int main(int argc, char* argv[]) {
+        if (argc != 4) {
+                std::cerr << "Usage: " << argv[0] << " <interface> <sender_ip> <target_ip>" << std::endl;
+                return -1;
+        }
+        std::signal(SIGINT, signalHandler);
+        try {
+                engine = std::make_unique<Engine>(argv[1], argv[2], argv[3]);
+                engine->run();
+        }
+        catch (const std::exception& e) {
+                std::cerr << "[Fatal Error] " << e.what() << std::endl;
+                return -1;
+        }
+
+        // engine 포인터가 스코프를 벗어나면서 소멸자 호출 -> recover() 자동 실행
+        std::cout << "[Main] Byte." << std::endl;
+        return 0;
+}
